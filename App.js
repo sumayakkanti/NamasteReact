31bb26d8dfabb934c3ff4caa97a190fc502f2d8e
Sumalatha Yakkanti <sumalathayakkanti@sumalathas-mbp.speedport.ip>
2023-07-27 15:59:28 +0200

    Inception

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"This is h1"),
+        React.createElement("h2",{},"This is h2")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"This is h1"),
+        React.createElement("h2",{},"This is h2")
+    ])
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
