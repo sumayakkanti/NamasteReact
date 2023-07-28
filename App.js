eb6be5c62b3593b6b36e8c71c0853257044485c9
Sumalatha Yakkanti <sumalathayakkanti@sumalathas-mbp.speedport.ip>
2023-07-28 10:32:55 +0200

    JSX intri

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactD from "react-dom"
-const parent = React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"This is h1"),
-        React.createElement("h2",{},"This is h2")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"This is h1"),
-        React.createElement("h2",{},"This is h2")
-    ])
-])
+import ReactDOM from "react-dom/client"
+
+const ParentComponent = () =>
+(
+    <div>
+        {heading}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const heading = <h1>This Namste React application</h1>
+
+root.render(<ParentComponent />);
